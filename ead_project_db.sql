8b9fa2e81a0c1c8a81328a5228099aee08c16a34
Darren Britton <darrenbritton@hotmail.com>
2016-04-23 16:59:19 +0100

    updated database name and added create db to sql file

--- ead_project_db.sql
+++ ead_project_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
+-- version 4.1.14
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 23, 2016 at 03:25 PM
--- Server version: 10.1.9-MariaDB
--- PHP Version: 5.6.15
+-- Generation Time: Apr 23, 2016 at 05:57 PM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,11 +14,13 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `ead_project`
 --
+CREATE DATABASE IF NOT EXISTS `ead_project` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `ead_project`;
 
 -- --------------------------------------------------------
 
@@ -26,22 +28,28 @@ SET time_zone = "+00:00";
 -- Table structure for table `moves`
 --
 
-CREATE TABLE `moves` (
-  `move_id` int(10) UNSIGNED NOT NULL,
+CREATE TABLE IF NOT EXISTS `moves` (
+  `move_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `move_name` varchar(30) NOT NULL,
   `accuracy` int(11) NOT NULL,
   `pp` int(11) NOT NULL,
-  `power` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `power` int(11) NOT NULL,
+  PRIMARY KEY (`move_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
+--
+-- Truncate table before insert `moves`
+--
+
+TRUNCATE TABLE `moves`;
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `pokemon`
 --
 
-CREATE TABLE `pokemon` (
-  `id` int(10) UNSIGNED NOT NULL COMMENT 'Usual Pokemon ID. 1: Bulbasaur etc',
+CREATE TABLE IF NOT EXISTS `pokemon` (
+  `id` int(10) unsigned NOT NULL COMMENT 'Usual Pokemon ID. 1: Bulbasaur etc',
   `name` varchar(30) NOT NULL,
   `height` int(11) NOT NULL,
   `weight` int(11) NOT NULL,
@@ -49,60 +57,36 @@ CREATE TABLE `pokemon` (
   `move1_id` int(11) NOT NULL,
   `move2_id` int(11) DEFAULT NULL,
   `move3_id` int(11) DEFAULT NULL,
-  `move4_id` int(11) DEFAULT NULL
+  `move4_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Truncate table before insert `pokemon`
+--
+
+TRUNCATE TABLE `pokemon`;
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `users`
 --
 
-CREATE TABLE `users` (
-  `id` int(10) UNSIGNED NOT NULL,
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL,
   `name` varchar(50) NOT NULL,
   `surname` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `password` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `password` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
--- Indexes for dumped tables
+-- Truncate table before insert `users`
 --
 
---
--- Indexes for table `moves`
---
-ALTER TABLE `moves`
-  ADD PRIMARY KEY (`move_id`);
-
---
--- Indexes for table `pokemon`
---
-ALTER TABLE `pokemon`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `moves`
---
-ALTER TABLE `moves`
-  MODIFY `move_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+TRUNCATE TABLE `users`;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
