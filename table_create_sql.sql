f6c2f1a98c15fa83ebcd4ff98ea7d5453a17765a
JosephDevaney <joe.devaney1@gmail.com>
2016-04-23 14:03:06 +0100

    Added sql to create pokemon table

--- table_create_sql.sql
+++ table_create_sql.sql
@@ -7,3 +7,7 @@ CREATE TABLE `users` (
  `password` varchar(50) NOT NULL,
  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1
+
+
+--Pokemon
+CREATE TABLE `ead_project`.`pokemon` ( `id` INT UNSIGNED NOT NULL COMMENT 'Usual Pokemon ID. 1: Bulbasaur etc' , `name` VARCHAR(30) NOT NULL , `height` INT NOT NULL , `weight` INT NOT NULL , `move1_id` INT NOT NULL , `move2_id` INT NOT NULL , `move3_id` INT NOT NULL , `move4_id` INT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
