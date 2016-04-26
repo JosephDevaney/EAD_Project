4f2cf46d0b3835d35863a8d1402a87ac3b64c496
JosephDevaney <joe.devaney1@gmail.com>
2016-04-26 14:29:10 +0100

    Fully refactored UserDAO to use BaseDAO methods

--- app/DB/DAO/UsersDAO.php
+++ app/DB/DAO/UsersDAO.php
@@ -3,48 +3,27 @@
  * @author Luca
  * definition of the User DAO (database access object)
  */
-class UsersDAO {
-	private $dbManager;
-	function UsersDAO($DBMngr) {
+require_once("DB/DAO/BaseDAO.php");
+class UsersDAO extends BaseDAO{
+    function __construct($DBmgr)
+    {
+        parent::__construct($DBmgr);
+    }
+
+    function UsersDAO($DBMngr) {
 		$this->dbManager = $DBMngr;
 	}
 	public function get($id = null) {
-		$values = array();
-        $sql = "SELECT * ";
-		$sql .= "FROM users ";
-		if ($id != null) {
-            $sql .= "WHERE users.id=? ";
-            $values = array($id => PDO::PARAM_INT);
-        }
-
-		$sql .= "ORDER BY users.name ";
-		
-//		$stmt = $this->dbManager->prepareQuery ( $sql );
-//		$this->dbManager->bindValue ( $stmt, 1, $id, $this->dbManager->INT_TYPE );
-        $stmt = $this->prepare_stmt($sql, $values);
-		$this->dbManager->executeQuery ( $stmt );
-		$rows = $this->dbManager->fetchResults ( $stmt );
-		
-		return ($rows);
+        return ($this->base_get($id, "users", "id", "name"));
 	}
 	public function insert($parametersArray) {
 		// insertion assumes that all the required parameters are defined and set
-		$sql = "INSERT INTO users (username, name, surname, email, password) ";
-		$sql .= "VALUES (?,?,?,?,?) ";
+        $sql = "INSERT INTO users (username, name, surname, email, password) ";
+        $sql .= "VALUES (?,?,?,?,?) ";
         $values = array($parametersArray["username"] => PDO::PARAM_STR, $parametersArray["name"] => PDO::PARAM_STR,
-            $parametersArray ["surname"] => PDO::PARAM_STR, $parametersArray ["email"] => PDO::PARAM_STR);
-        $values[$parametersArray["password"]] = PDO::PARAM_STR;
-        $stmt = $this->prepare_stmt($sql, $values);
-
-//		$stmt = $this->dbManager->prepareQuery ( $sql );
-//		$this->dbManager->bindValue ( $stmt, 1, $parametersArray ["username"], $this->dbManager->STRING_TYPE );
-//		$this->dbManager->bindValue ( $stmt, 2, $parametersArray ["name"], $this->dbManager->STRING_TYPE );
-//		$this->dbManager->bindValue ( $stmt, 3, $parametersArray ["surname"], $this->dbManager->STRING_TYPE );
-//		$this->dbManager->bindValue ( $stmt, 4, $parametersArray ["email"], $this->dbManager->STRING_TYPE );
-//		$this->dbManager->bindValue ( $stmt, 5, $parametersArray ["password"], $this->dbManager->STRING_TYPE );
-		$this->dbManager->executeQuery ( $stmt );
+            $parametersArray ["surname"] => PDO::PARAM_STR, $parametersArray ["email"] => PDO::PARAM_STR, $parametersArray["password"] => PDO::PARAM_STR);
 		
-		return ($this->dbManager->getLastInsertedID ());
+		return ($this->base_insert($sql, $values));
 	}
 	public function update($parametersArray, $userID) {
 		$sql = 'UPDATE users SET username=?, name=?, surname=?,email=?,password=? WHERE id=?';
@@ -53,42 +32,18 @@ class UsersDAO {
         $values = array($parametersArray["username"] => PDO::PARAM_STR, $parametersArray ["name"] => PDO::PARAM_STR,
             $parametersArray ["surname"] => PDO::PARAM_STR, $parametersArray ["email"] => PDO::PARAM_STR,
             $parametersArray ["password"] => PDO::PARAM_STR, $userID => PDO::PARAM_INT);
-        $stmt = $this->prepare_stmt($sql, $values);
-//		$stmt = $this->dbManager->prepareQuery($sql);
-//		$this->dbManager->bindValue($stmt, 1, $parametersArray ["username"], PDO::PARAM_STR);
-//		$this->dbManager->bindValue($stmt, 2, $parametersArray["name"], PDO::PARAM_STR);
-//		$this->dbManager->bindValue($stmt, 3, $parametersArray["surname"], PDO::PARAM_STR);
-//		$this->dbManager->bindValue($stmt, 4, $parametersArray["email"], PDO::PARAM_STR);
-//		$this->dbManager->bindValue($stmt, 5, $parametersArray["password"], PDO::PARAM_STR);
-//		$this->dbManager->bindValue($stmt, 6, $userID, PDO::PARAM_INT);
-		$res = $this->dbManager->executeQuery ( $stmt );
+
+        $this->base_update($sql, $values);
 
 		return $userID;
 	}
 	public function delete($userID) {
-		//TODO
-		$sql = 'DELETE FROM users WHERE id=?';
-		$sql .= ';';
-
-//		$stmt = $this->dbManager->prepareQuery($sql);
-//		$this->dbManager->bindValue($stmt, 1, $userID, PDO::PARAM_INT);
-        $stmt = $this->prepare_stmt($sql, array($userID => PDO::PARAM_INT));
-		$this->dbManager->executeQuery ( $stmt );
-
-		return $userID;
+		return $this->base_delete("users", "id", $userID);
 	}
 	public function purge() {
-		//TODO
-		$sql = 'TRUNCATE users';
-		$sql .= ';';
-
-		$stmt = $this->dbManager->prepareQuery($sql);
-		$this->dbManager->executeQuery ( $stmt );
-
-		return true;
+		return $this->base_purge("users");
 	}
 	public function search($str) {
-		//TODO
 		$sql = "SELECT * ";
 		$sql .= "FROM users ";
 		$sql .= "WHERE NAME LIKE %?% ";
@@ -96,15 +51,7 @@ class UsersDAO {
 		$sql .= "ORDER BY users.name ";
 		$sql .= ";";
 
-//		$stmt = $this->dbManager->prepareQuery( $sql );
-//		$this->dbManager->bindValue($stmt, 1, "%".$str."%", PDO::PARAM_STR);
-//		$this->dbManager->bindValue($stmt, 2, "%".$str."%", PDO::PARAM_STR);
-
-        $stmt = $this->prepare_stmt($sql, array($str => PDO::PARAM_STR, $str => PDO::PARAM_STR));
-		$this->dbManager->executeQuery ( $stmt );
-
-		$arrayOfResults = $this->dbManager->fetchResults ( $stmt );
-		return $arrayOfResults;
+		return $this->base_search($sql, array($str => PDO::PARAM_STR, $str => PDO::PARAM_STR));
 	}
 	
 	public function searchUsername($str) {
@@ -114,24 +61,8 @@ class UsersDAO {
 		$sql .= "WHERE USERNAME = ? ";
 		$sql .= "ORDER BY users.name ";
 		$sql .= ";";
-	
-//		$stmt = $this->dbManager->prepareQuery( $sql );
-//		$this->dbManager->bindValue($stmt, 1, $str, PDO::PARAM_STR);
-        $stmt = $this->prepare_stmt($sql, array($str => PDO::PARAM_STR));
-		$this->dbManager->executeQuery ( $stmt );
 
-		$arrayOfResults = $this->dbManager->fetchResults ( $stmt );
-		return $arrayOfResults;
+		return $this->base_search($sql, array($str => PDO::PARAM_STR));
 	}
 
-    private function prepare_stmt($sql, $values) {
-        $stmt = $this->dbManager->prepareQuery($sql);
-        $i = 1;
-        foreach($values as $value => $type)
-            $this->dbManager->bindValue($stmt, $i++, $value, $type);
-
-        return ($stmt);
-    }
 }
-
-?>
