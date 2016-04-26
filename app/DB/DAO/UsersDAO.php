3cc9fd6dcfb9eca73d8369a70cd1cbe5b46afd96
JosephDevaney <joe.devaney1@gmail.com>
2016-04-26 13:35:32 +0100

    Started refactoring DAO classes

--- app/DB/DAO/UsersDAO.php
+++ app/DB/DAO/UsersDAO.php
@@ -9,14 +9,19 @@ class UsersDAO {
 		$this->dbManager = $DBMngr;
 	}
 	public function get($id = null) {
-		$sql = "SELECT * ";
+		$values = array();
+        $sql = "SELECT * ";
 		$sql .= "FROM users ";
-		if ($id != null)
-			$sql .= "WHERE users.id=? ";
+		if ($id != null) {
+            $sql .= "WHERE users.id=? ";
+            $values = array($id => PDO::PARAM_INT);
+        }
+
 		$sql .= "ORDER BY users.name ";
 		
-		$stmt = $this->dbManager->prepareQuery ( $sql );
-		$this->dbManager->bindValue ( $stmt, 1, $id, $this->dbManager->INT_TYPE );
+//		$stmt = $this->dbManager->prepareQuery ( $sql );
+//		$this->dbManager->bindValue ( $stmt, 1, $id, $this->dbManager->INT_TYPE );
+        $stmt = $this->prepare_stmt($sql, $values);
 		$this->dbManager->executeQuery ( $stmt );
 		$rows = $this->dbManager->fetchResults ( $stmt );
 		
@@ -26,43 +31,50 @@ class UsersDAO {
 		// insertion assumes that all the required parameters are defined and set
 		$sql = "INSERT INTO users (username, name, surname, email, password) ";
 		$sql .= "VALUES (?,?,?,?,?) ";
-		
-		$stmt = $this->dbManager->prepareQuery ( $sql );
-		$this->dbManager->bindValue ( $stmt, 1, $parametersArray ["username"], $this->dbManager->STRING_TYPE );
-		$this->dbManager->bindValue ( $stmt, 2, $parametersArray ["name"], $this->dbManager->STRING_TYPE );
-		$this->dbManager->bindValue ( $stmt, 3, $parametersArray ["surname"], $this->dbManager->STRING_TYPE );
-		$this->dbManager->bindValue ( $stmt, 4, $parametersArray ["email"], $this->dbManager->STRING_TYPE );
-		$this->dbManager->bindValue ( $stmt, 5, $parametersArray ["password"], $this->dbManager->STRING_TYPE );
+        $values = array($parametersArray["username"] => PDO::PARAM_STR, $parametersArray["name"] => PDO::PARAM_STR,
+            $parametersArray ["surname"] => PDO::PARAM_STR, $parametersArray ["email"] => PDO::PARAM_STR);
+        $values[$parametersArray["password"]] = PDO::PARAM_STR;
+        $stmt = $this->prepare_stmt($sql, $values);
+
+//		$stmt = $this->dbManager->prepareQuery ( $sql );
+//		$this->dbManager->bindValue ( $stmt, 1, $parametersArray ["username"], $this->dbManager->STRING_TYPE );
+//		$this->dbManager->bindValue ( $stmt, 2, $parametersArray ["name"], $this->dbManager->STRING_TYPE );
+//		$this->dbManager->bindValue ( $stmt, 3, $parametersArray ["surname"], $this->dbManager->STRING_TYPE );
+//		$this->dbManager->bindValue ( $stmt, 4, $parametersArray ["email"], $this->dbManager->STRING_TYPE );
+//		$this->dbManager->bindValue ( $stmt, 5, $parametersArray ["password"], $this->dbManager->STRING_TYPE );
 		$this->dbManager->executeQuery ( $stmt );
 		
 		return ($this->dbManager->getLastInsertedID ());
 	}
 	public function update($parametersArray, $userID) {
-		//TODO
 		$sql = 'UPDATE users SET username=?, name=?, surname=?,email=?,password=? WHERE id=?';
 		$sql .= ';';
-		
-		$stmt = $this->dbManager->prepareQuery($sql);
-		$this->dbManager->bindValue($stmt, 1, $parametersArray ["username"], PDO::PARAM_STR);
-		$this->dbManager->bindValue($stmt, 2, $parametersArray["name"], PDO::PARAM_STR);
-		$this->dbManager->bindValue($stmt, 3, $parametersArray["surname"], PDO::PARAM_STR);
-		$this->dbManager->bindValue($stmt, 4, $parametersArray["email"], PDO::PARAM_STR);
-		$this->dbManager->bindValue($stmt, 5, $parametersArray["password"], PDO::PARAM_STR);
-		$this->dbManager->bindValue($stmt, 6, $userID, PDO::PARAM_INT);
+
+        $values = array($parametersArray["username"] => PDO::PARAM_STR, $parametersArray ["name"] => PDO::PARAM_STR,
+            $parametersArray ["surname"] => PDO::PARAM_STR, $parametersArray ["email"] => PDO::PARAM_STR,
+            $parametersArray ["password"] => PDO::PARAM_STR, $userID => PDO::PARAM_INT);
+        $stmt = $this->prepare_stmt($sql, $values);
+//		$stmt = $this->dbManager->prepareQuery($sql);
+//		$this->dbManager->bindValue($stmt, 1, $parametersArray ["username"], PDO::PARAM_STR);
+//		$this->dbManager->bindValue($stmt, 2, $parametersArray["name"], PDO::PARAM_STR);
+//		$this->dbManager->bindValue($stmt, 3, $parametersArray["surname"], PDO::PARAM_STR);
+//		$this->dbManager->bindValue($stmt, 4, $parametersArray["email"], PDO::PARAM_STR);
+//		$this->dbManager->bindValue($stmt, 5, $parametersArray["password"], PDO::PARAM_STR);
+//		$this->dbManager->bindValue($stmt, 6, $userID, PDO::PARAM_INT);
 		$res = $this->dbManager->executeQuery ( $stmt );
-		
-//		var_dump($this->dbManager->getLastInsertedID());
+
 		return $userID;
 	}
 	public function delete($userID) {
 		//TODO
 		$sql = 'DELETE FROM users WHERE id=?';
 		$sql .= ';';
-		
-		$stmt = $this->dbManager->prepareQuery($sql);
-		$this->dbManager->bindValue($stmt, 1, $userID, PDO::PARAM_INT);
-		$res = $this->dbManager->executeQuery ( $stmt );
-		
+
+//		$stmt = $this->dbManager->prepareQuery($sql);
+//		$this->dbManager->bindValue($stmt, 1, $userID, PDO::PARAM_INT);
+        $stmt = $this->prepare_stmt($sql, array($userID => PDO::PARAM_INT));
+		$this->dbManager->executeQuery ( $stmt );
+
 		return $userID;
 	}
 	public function purge() {
@@ -79,16 +91,18 @@ class UsersDAO {
 		//TODO
 		$sql = "SELECT * ";
 		$sql .= "FROM users ";
-		$sql .= "WHERE NAME LIKE ? ";
-		$sql .= "OR SURNAME LIKE ? ";
+		$sql .= "WHERE NAME LIKE %?% ";
+		$sql .= "OR SURNAME LIKE %?% ";
 		$sql .= "ORDER BY users.name ";
 		$sql .= ";";
-		
-		$stmt = $this->dbManager->prepareQuery( $sql );
-		$this->dbManager->bindValue($stmt, 1, "%".$str."%", PDO::PARAM_STR);
-		$this->dbManager->bindValue($stmt, 2, "%".$str."%", PDO::PARAM_STR);
+
+//		$stmt = $this->dbManager->prepareQuery( $sql );
+//		$this->dbManager->bindValue($stmt, 1, "%".$str."%", PDO::PARAM_STR);
+//		$this->dbManager->bindValue($stmt, 2, "%".$str."%", PDO::PARAM_STR);
+
+        $stmt = $this->prepare_stmt($sql, array($str => PDO::PARAM_STR, $str => PDO::PARAM_STR));
 		$this->dbManager->executeQuery ( $stmt );
-		
+
 		$arrayOfResults = $this->dbManager->fetchResults ( $stmt );
 		return $arrayOfResults;
 	}
@@ -101,12 +115,23 @@ class UsersDAO {
 		$sql .= "ORDER BY users.name ";
 		$sql .= ";";
 	
-		$stmt = $this->dbManager->prepareQuery( $sql );
-		$this->dbManager->bindValue($stmt, 1, $str, PDO::PARAM_STR);
+//		$stmt = $this->dbManager->prepareQuery( $sql );
+//		$this->dbManager->bindValue($stmt, 1, $str, PDO::PARAM_STR);
+        $stmt = $this->prepare_stmt($sql, array($str => PDO::PARAM_STR));
 		$this->dbManager->executeQuery ( $stmt );
-	
+
 		$arrayOfResults = $this->dbManager->fetchResults ( $stmt );
 		return $arrayOfResults;
 	}
+
+    private function prepare_stmt($sql, $values) {
+        $stmt = $this->dbManager->prepareQuery($sql);
+        $i = 1;
+        foreach($values as $value => $type)
+            $this->dbManager->bindValue($stmt, $i++, $value, $type);
+
+        return ($stmt);
+    }
 }
+
 ?>
--- app/models/UserModel.php
+++ app/models/UserModel.php
@@ -52,19 +52,19 @@ class UserModel {
 	}
 	public function updateUsers($userID, $newUserRepresentation) {
 		//TODO
-		var_dump($newUserRepresentation);
+//		var_dump($newUserRepresentation);
 		if (! empty ( $newUserRepresentation ["name"] ) && ! empty ( $newUserRepresentation ["surname"] ) && ! empty ( $newUserRepresentation ["email"] ) && ! empty ( $newUserRepresentation ["password"] )) {
 			/*
 			 * the model knows the representation of a user in the database and this is: name: varchar(25) surname: varchar(25) email: varchar(50) password: varchar(40)
 			 */
-					var_dump($newUserRepresentation);
+//					var_dump($newUserRepresentation);
 				
 			if (($this->validationSuite->isLengthStringValid ( $newUserRepresentation ["name"], TABLE_USER_NAME_LENGTH )) &&
 					($this->validationSuite->isLengthStringValid ( $newUserRepresentation ["surname"], TABLE_USER_SURNAME_LENGTH )) &&
 					($this->validationSuite->isLengthStringValid ( $newUserRepresentation ["email"], TABLE_USER_EMAIL_LENGTH )) &&
 					($this->validationSuite->isLengthStringValid ( $newUserRepresentation ["password"], TABLE_USER_PASSWORD_LENGTH ))) {
 						if ($userID = $this->UsersDAO->update( $newUserRepresentation, $userID ))
-							var_dump($userID);
+//							var_dump($userID);
 							return ($userID);
 					}
 		}
