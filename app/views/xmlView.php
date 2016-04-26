0c20dc86d718b371e0f547704a57de14369840f7
JosephDevaney <joe.devaney1@gmail.com>
2016-04-26 15:38:47 +0100

    Added simple XML encoder

--- app/views/xmlView.php
+++ app/views/xmlView.php
@@ -9,14 +9,38 @@ class xmlView
         $this->controller = $controller;
         $this->model = $model;
         $this->slimApp = $slimApp;
-        $this->xmlEncoder = new XmlEncoder($this->model->apiResponse);
+//        $this->xmlEncoder = new XmlEncoder($this->model->apiResponse);
     }
 
     public function output(){
         //prepare xml response
-        $this->xmlEncoder->encode();
-        if($response = $this->xmlEncoder->getUnformattedString())
-            $this->slimApp->response->write($response);
+        $xml_str = "";
+        $resp = $this->model->apiResponse;
+        if (is_array($resp)) {
+            foreach ($resp as $obj) {
+                $xml_str .= "<Object>";
+                $xml_str .= "\n";
+                foreach ($obj as $key => $value) {
+                    $xml_str .= "\t";
+                    $xml_str .= "<" . $key . ">";
+                    $xml_str .= $value;
+                    $xml_str .= "</" . $key . ">";
+                    $xml_str .= "\n";
+                }
+                $xml_str .= "</Object>";
+                $xml_str .= "\n";
+            }
+        }
+        else
+            $xml_str .= $resp;
+
+        $this->slimApp->response->write($xml_str);
+
+
+//        var_dump($this->model->apiResponse);
+//        $this->xmlEncoder->encode();
+//        if($response = $this->xmlEncoder->getUnformattedString())
+//            $this->slimApp->response->write($response);
     }
 
 }
