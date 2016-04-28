9b62b5964df323ca341bc940e39fe7af71a7554a
JosephDevaney <joe.devaney1@gmail.com>
2016-04-28 22:14:09 +0100

    Updated Pokemon and User controller classes

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -1,38 +1,35 @@
 <?php
-class UserController {
-	private $slimApp;
-	private $model;
-	private $requestBody;
+
+require_once ("controllers/BaseController.php");
+class UserController extends BaseController{
 	public function __construct($model, $action = null, $slimApp, $parameters = null) {
-		$this->model = $model;
-		$this->slimApp = $slimApp;
-		$this->requestBody = json_decode ( $this->slimApp->request->getBody (), true ); // this must contain the representation of the new user
+        parent::__construct($model, $slimApp);
 		
 		if (! empty ( $parameters ["id"] ))
 			$id = $parameters ["id"];
 		
 		switch ($action) {
 			case ACTION_GET_USER :
-				$this->getUser ( $id );
+				$this->get ( $id );
 				break;
 			case ACTION_GET_USERS :
-				$this->getUsers ();
+				$this->getAll ();
 				break;
 			case ACTION_UPDATE_USER :
-				$this->updateUser ( $id, $this->requestBody );
+				$this->update ( $id, $this->requestBody );
 				break;
 			case ACTION_CREATE_USER :
-				$this->createNewUser ( $this->requestBody );
+				$this->insert ( $this->requestBody );
 				break;
 			case ACTION_DELETE_USER :
-				$this->deleteUser ( $id );
+				$this->delete ( $id );
 				break;
 			case ACTION_SEARCH_USERS :
 				$string = $parameters ["SearchingString"];
-				$this->searchUsers ( $string );
+				$this->search ( $string );
 				break;
             case ACTION_PURGE_USERS :
-                $this->purgeUsers();
+                $this->purge();
                 break;
 			case ACTION_AUTHENTICATE_USER :
 				$this->authenticateUser($parameters);
@@ -46,116 +43,7 @@ class UserController {
 				break;
 		}
 	}
-	private function getUsers() {
-		$answer = $this->model->getUsers ();
-		if ($answer != null) {
-			$this->slimApp->response ()->setStatus ( HTTPSTATUS_OK );
-			$this->model->apiResponse = $answer;
-		} else {
-			$this->slimApp->response ()->setStatus ( HTTPSTATUS_NOCONTENT );
-			$Message = array (
-					GENERAL_MESSAGE_LABEL => GENERAL_NOCONTENT_MESSAGE 
-			);
-			$this->model->apiResponse = $Message;
-		}
-	}
 
-	private function getUser($userID) {
-		$answer = $this->model->getUser ( $userID );
-		if ($answer != null) {
-			$this->slimApp->response ()->setStatus ( HTTPSTATUS_OK );
-			$this->model->apiResponse = $answer;
-		} else {
-			
-			$this->slimApp->response ()->setStatus ( HTTPSTATUS_NOCONTENT );
-			$Message = array (
-					GENERAL_MESSAGE_LABEL => GENERAL_NOCONTENT_MESSAGE 
-			);
-			$this->model->apiResponse = $Message;
-		}
-	}
-	
-	private function createNewUser($newUser) {
-		if ($newID = $this->model->createNewUser ( $newUser )) {
-			$this->slimApp->response ()->setStatus ( HTTPSTATUS_CREATED );
-			$Message = array (
-					GENERAL_MESSAGE_LABEL => GENERAL_RESOURCE_CREATED,
-					"id" => "$newID" 
-			);
-			$this->model->apiResponse = $Message;
-		} else {
-			$this->slimApp->response ()->setStatus ( HTTPSTATUS_BADREQUEST );
-			$Message = array (
-					GENERAL_MESSAGE_LABEL => GENERAL_INVALIDBODY 
-			);
-			$this->model->apiResponse = $Message;
-		}
-	}
-	private function deleteUser($userId) {
-		//TODO
-		if ($newID = $this->model->deleteUser ( $userId )) {
-			$this->slimApp->response ()->setStatus ( HTTPSTATUS_OK);
-			$Message = array (
-					GENERAL_MESSAGE_LABEL => GENERAL_RESOURCE_DELETED,
-					"id" => "$userId"
-			);
-			$this->model->apiResponse = $Message;
-		} else {
-			$this->slimApp->response ()->setStatus ( HTTPSTATUS_BADREQUEST );
-			$Message = array (
-					GENERAL_MESSAGE_LABEL => GENERAL_INVALIDBODY
-			);
-			$this->model->apiResponse = $Message;
-		}
-	}
-	
-	private function updateUser($userID, $parameters) {
-		//TODO
-		if ($userID = $this->model->updateUsers ( $userID, $parameters )) {
-			$this->slimApp->response ()->setStatus ( HTTPSTATUS_CREATED );
-			$Message = array (
-					GENERAL_MESSAGE_LABEL => GENERAL_RESOURCE_UPDATED,
-					"id" => "$userID"
-			);
-			$this->model->apiResponse = $Message;
-		} else {
-			$this->slimApp->response ()->setStatus ( HTTPSTATUS_BADREQUEST );
-			$Message = array (
-					GENERAL_MESSAGE_LABEL => GENERAL_NOTMODIFIED_MESSAGE
-			);
-			$this->model->apiResponse = $Message;
-		}
-	}
-	private function searchUsers($string) {
-		//TODO
-		if ($users = $this->model->searchUsers ( $string )) {
-			$this->slimApp->response ()->setStatus ( HTTPSTATUS_OK );
-			
-			$this->model->apiResponse = $users;
-		} else {
-			$this->slimApp->response ()->setStatus ( HTTPSTATUS_BADREQUEST );
-			$Message = array (
-					GENERAL_MESSAGE_LABEL => GENERAL_NOCONTENT_MESSAGE
-			);
-			$this->model->apiResponse = $Message;
-		}
-	}
-    private function purgeUsers() {
-        //TODO
-        if ($this->model->purgeUsers()) {
-            $this->slimApp->response ()->setStatus ( HTTPSTATUS_OK );
-            $Message = array (
-                GENERAL_MESSAGE_LABEL => GENERAL_PURGE_MESSAGE
-            );
-            $this->model->apiResponse = $Message;
-        } else {
-            $this->slimApp->response ()->setStatus ( HTTPSTATUS_BADREQUEST );
-            $Message = array (
-                GENERAL_MESSAGE_LABEL => GENERAL_NOCONTENT_MESSAGE
-            );
-            $this->model->apiResponse = $Message;
-        }
-    }
 	private function authenticateUser($params) {
 		if ($this->model->authenticateUser ( $params )) {
 			$this->slimApp->response->headers->set('Authenticated', 'true');
@@ -169,4 +57,3 @@ class UserController {
 		}
 	}
 }
-?>
